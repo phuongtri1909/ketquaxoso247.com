04f7f00b24297aaa900b868095c8ef5e3ae6b996
phuongtri1909 <tri2003bt@gmail.com>
2025-04-17 23:28:51 +0700

    update

--- resources/views/widgets/frontend_menu_right.blade.php
+++ resources/views/widgets/frontend_menu_right.blade.php
@@ -17,30 +17,28 @@
 
         </div>
 
-        <div class="content-right bullet tk-block live_mb">
-            <div class="title-r"><a class="bg-blue" title="Xổ số vietlott" href="{{ route('vietlott') }}">Xổ số
-                    vietlott</a></div>
+        <div class="content-right bullet live_mt">
+            <div class="title-r"><a class="bg-blue" title="Xổ số miền trung" href="{{ route('xsmt') }}">Xổ
+                    số miền trung</a></div>
             <ul>
-                <li @if (url()->full() == route('xsthantai')) class="active" @endif><a href="{{ route('xsthantai') }}">Thần
-                        Tài
-                    </a></li>
-                <li @if (url()->full() == route('dientoan123')) class="active" @endif><a href="{{ route('dientoan123') }}">Điện
-                        toán 123
-                    </a></li>
-                <li @if (url()->full() == route('dientoan6x36')) class="active" @endif><a href="{{ route('dientoan6x36') }}">Điện
-                        toán 6x36</a></li>
-                {{-- <li @if (url()->full() == route('max3d')) class="active" @endif><a href="#">Max 3D</a> --}}
-                {{-- </li> --}}
-                {{-- <li @if (url()->full() == route('max3dpro')) class="active" @endif><a href="#">Max 3D --}}
-                {{-- Pro --}}
-                {{-- </a></li> --}}
-                <li @if (url()->full() == route('mega645')) class="active" @endif><a href="{{ route('mega645') }}">Mega
-                        6/45</a></li>
-                <li @if (url()->full() == route('power655')) class="active" @endif><a href="{{ route('power655') }}">Power
-                        6/55
-                    </a></li>
+                @foreach ($mt_province as $pro)
+                    @if (strpos($pro->ngay_quay, $day) !== false)
+                        <li @if (url()->full() == route('xstinh.tinh', $pro->slug)) class="active" @endif>
+                            <a href="{{ route('xstinh.tinh', $pro->slug) }}"
+                                title="{{ $pro->name }}">{{ $pro->name }}</a>
+                            <i class="icon icon_live" style="display: none"><i
+                                    class="fas fa-spinner fa-pulse text-danger"></i></i>
+                            <i class="fas fa-check icon icon_done" style="display: none"></i>
+                            <span class="badge icon icon_w" style="display: none">17:05</span>
+                        </li>
+                    @else
+                        <li @if (url()->full() == route('xstinh.tinh', $pro->slug)) class="active" @endif><a
+                                href="{{ route('xstinh.tinh', $pro->slug) }}"
+                                @if (url()->full() == route('xstinh.tinh', $pro->slug)) @else class="list-group-item" @endif
+                                title="{{ $pro->name }}">{{ $pro->name }}</a></li>
+                    @endif
+                @endforeach
             </ul>
-
         </div>
 
         <div class="content-right bullet live_mn">
@@ -68,59 +66,33 @@
             </ul>
         </div>
 
-        <div class="content-right bullet live_mt">
-            <div class="title-r"><a class="bg-blue" title="Xổ số miền trung" href="{{ route('xsmt') }}">Xổ
-                    số miền trung</a></div>
-            <ul>
-                @foreach ($mt_province as $pro)
-                    @if (strpos($pro->ngay_quay, $day) !== false)
-                        <li @if (url()->full() == route('xstinh.tinh', $pro->slug)) class="active" @endif>
-                            <a href="{{ route('xstinh.tinh', $pro->slug) }}"
-                                title="{{ $pro->name }}">{{ $pro->name }}</a>
-                            <i class="icon icon_live" style="display: none"><i
-                                    class="fas fa-spinner fa-pulse text-danger"></i></i>
-                            <i class="fas fa-check icon icon_done" style="display: none"></i>
-                            <span class="badge icon icon_w" style="display: none">17:05</span>
-                        </li>
-                    @else
-                        <li @if (url()->full() == route('xstinh.tinh', $pro->slug)) class="active" @endif><a
-                                href="{{ route('xstinh.tinh', $pro->slug) }}"
-                                @if (url()->full() == route('xstinh.tinh', $pro->slug)) @else class="list-group-item" @endif
-                                title="{{ $pro->name }}">{{ $pro->name }}</a></li>
-                    @endif
-                @endforeach
-            </ul>
-        </div>
-        <div class="content-right dd-xs">
-            <div class="title-r"><a class="bg-blue" href="{{ route('dudoan.xsmb') }}" title="Dự đoán xổ số">Dự
-                    đoán xổ số</a></div>
-            <ul class="list-news">
-                @foreach ($postTK as $item)
-                    <?php
-                    $link = str_replace('xoso.site', 'ketquaxoso247.com', $item->link);
-                    
-                    ?>
-                    <span class="fw-bold ms-2 ">
-                        <i class="fa-regular fa-hand-point-right" style="color: #d05f43;"></i>
-                        @if ($item->category_id == '1')
-                            <a class="text-primary fw-bold" href="{{ route('dudoan.xsmb') }}" title="Dự đoán xổ số miền Bắc">Dự đoán XSMB</a>
-                        @elseif ($item->category_id == '3')
-                            <a class="text-primary fw-bold" href="{{ route('dudoan.xsmn') }}" title="Dự đoán xổ số miền Nam">Dự đoán XSMN</a>
-                        @elseif ($item->category_id == '2')
-                            <a class="text-primary fw-bold" href="{{ route('dudoan.xsmt') }}" title="Dự đoán xổ số miền Trung">Dự đoán XSMT</a>
-                        @endif
-                    </span>
-                    <li class="clearfix"><a title="{{ $item->title }}" href="{{ $link }}" class="fl"><img
-                                class="mag-r5 fl lazy" width="60" height="33" title="{{ $item->title }}"
-                                alt="{{ $item->title }}" src="{{ $item->img }}"
-                                data-src="{{ $item->img }}"></a><a href="{{ $link }}"
-                            title="{{ $item->title }}">{{ $item->title }}</a></li>
-                @endforeach
-            </ul>
-        </div>
+       
 
-    </div>
-    <div class="col-right">
+        <div class="content-right bullet tk-block live_mb">
+            <div class="title-r"><a class="bg-blue" title="Xổ số vietlott" href="{{ route('vietlott') }}">Xổ số
+                    vietlott</a></div>
+            <ul>
+                <li @if (url()->full() == route('xsthantai')) class="active" @endif><a href="{{ route('xsthantai') }}">Thần
+                        Tài
+                    </a></li>
+                <li @if (url()->full() == route('dientoan123')) class="active" @endif><a href="{{ route('dientoan123') }}">Điện
+                        toán 123
+                    </a></li>
+                <li @if (url()->full() == route('dientoan6x36')) class="active" @endif><a href="{{ route('dientoan6x36') }}">Điện
+                        toán 6x36</a></li>
+                {{-- <li @if (url()->full() == route('max3d')) class="active" @endif><a href="#">Max 3D</a> --}}
+                {{-- </li> --}}
+                {{-- <li @if (url()->full() == route('max3dpro')) class="active" @endif><a href="#">Max 3D --}}
+                {{-- Pro --}}
+                {{-- </a></li> --}}
+                <li @if (url()->full() == route('mega645')) class="active" @endif><a href="{{ route('mega645') }}">Mega
+                        6/45</a></li>
+                <li @if (url()->full() == route('power655')) class="active" @endif><a href="{{ route('power655') }}">Power
+                        6/55
+                    </a></li>
+            </ul>
+
+        </div>
 
         <div class="content-right bullet tk-cau ">
             <div class="title-r"><a class="bg-blue" title="Dò vé số online - May mắn mỗi ngày!...">Dò vé số online - May
@@ -159,6 +131,52 @@
                     </div>
                 </form>
             </div>
+        </div>
+
+        <div class="content-right bullet tk-block ">
+            <div class="title-r"><a class="bg-blue" title="Thống kê">Quay thử</a></div>
+            <ul class="stastic-lotery">
+                <li @if (url()->full() == route('quay_thu.mb')) class="active" @endif><a href="{{ route('quay_thu.mb') }}"
+                        title="Quay thử miền Bắc">Quay thử miền Bắc</a></li>
+                <li @if (url()->full() == route('quay_thu.mt')) class="active" @endif><a href="{{ route('quay_thu.mt') }}"
+                        title="Quay thử miền Trung">Quay thử miền Trung</a>
+                </li>
+                <li @if (url()->full() == route('quay_thu.mn')) class="active" @endif><a href="{{ route('quay_thu.mn') }}"
+                        title="Quay thử miền Nam">Quay thử miền Nam</a></li>
+            </ul>
+        </div>
+
+        
+
+    </div>
+    <div class="col-right">
+
+        <div class="content-right dd-xs">
+            <div class="title-r"><a class="bg-blue" href="{{ route('dudoan.xsmb') }}" title="Dự đoán xổ số">Dự
+                    đoán xổ số</a></div>
+            <ul class="list-news">
+                @foreach ($postTK as $item)
+                    <?php
+                    $link = str_replace('xoso.site', 'ketquaxoso247.com', $item->link);
+                    
+                    ?>
+                    <span class="fw-bold ms-2 ">
+                        <i class="fa-regular fa-hand-point-right" style="color: #d05f43;"></i>
+                        @if ($item->category_id == '1')
+                            <a class="text-primary fw-bold" href="{{ route('dudoan.xsmb') }}" title="Dự đoán xổ số miền Bắc">Dự đoán XSMB</a>
+                        @elseif ($item->category_id == '3')
+                            <a class="text-primary fw-bold" href="{{ route('dudoan.xsmn') }}" title="Dự đoán xổ số miền Nam">Dự đoán XSMN</a>
+                        @elseif ($item->category_id == '2')
+                            <a class="text-primary fw-bold" href="{{ route('dudoan.xsmt') }}" title="Dự đoán xổ số miền Trung">Dự đoán XSMT</a>
+                        @endif
+                    </span>
+                    <li class="clearfix"><a title="{{ $item->title }}" href="{{ $link }}" class="fl"><img
+                                class="mag-r5 fl lazy" width="60" height="33" title="{{ $item->title }}"
+                                alt="{{ $item->title }}" src="{{ $item->img }}"
+                                data-src="{{ $item->img }}"></a><a href="{{ $link }}"
+                            title="{{ $item->title }}">{{ $item->title }}</a></li>
+                @endforeach
+            </ul>
         </div>
 
         <div class="content-right bullet live_mb">
@@ -206,18 +224,7 @@
             </ul>
         </div>
 
-        <div class="content-right bullet tk-block ">
-            <div class="title-r"><a class="bg-blue" title="Thống kê">Quay thử</a></div>
-            <ul class="stastic-lotery">
-                <li @if (url()->full() == route('quay_thu.mb')) class="active" @endif><a href="{{ route('quay_thu.mb') }}"
-                        title="Quay thử miền Bắc">Quay thử miền Bắc</a></li>
-                <li @if (url()->full() == route('quay_thu.mt')) class="active" @endif><a href="{{ route('quay_thu.mt') }}"
-                        title="Quay thử miền Trung">Quay thử miền Trung</a>
-                </li>
-                <li @if (url()->full() == route('quay_thu.mn')) class="active" @endif><a href="{{ route('quay_thu.mn') }}"
-                        title="Quay thử miền Nam">Quay thử miền Nam</a></li>
-            </ul>
-        </div>
+        
 
 
         {{-- <div class="content-right bullet"> --}}
